c15d471aade512d7634eea20b1f09389c16ba4b7
Maickol Rivera <m4icol.r12@gmail.com>
2026-09-26 12:22:43 -0500

    refactor: simplify project section by deleting Slider component

--- src/sections/Projects.tsx
+++ src/sections/Projects.tsx
@@ -1,22 +1,14 @@
-import { useState } from "react";
 import ProjectsCode from "../assets/icons/section_icons/ProjectsIcon";
-import SwitchOption from "../components/SwitchOption";
-import ColumnsListIcon from "../assets/icons/switch_icons/ColumnsListIcon";
 import StarBackground from "../components/StarBackground";
-import SliderListIcon from "../assets/icons/switch_icons/SliderListIcon";
 import ColumnList from "./sub_sections/ColumnList";
-import SliderList from "./sub_sections/SliderList";
 
 export default function Projects(){
-
-    type List = "SLIDER" | "COLUMNS";
-    const [typeList, setTypeList] = useState<List>("SLIDER");
 
     return(
         <section 
         id="projects"
         className="
-        flex items-center gap-5 flex-col 
+        flex items-center gap-10 flex-col 
         px-5 w-full relative pt-25 my-60
         md:pt-40">
 
@@ -27,40 +19,7 @@ export default function Projects(){
                     </h2>
             </div>
 
-            <p className="max-w-[20rem] md:max-w-[28rem] text-sm text-gradient text-center opacity-80 md:text-base/5">
-                He desarrollando productos como landing pages, experiencias 3D interactivas, integraciones de encriptación, visualizacion de algoritmos y apps multiplataforma. 
-            </p>
-
-            <div className="
-                flex justify-between 
-                w-full mt-12 max-w-[20rem] 
-                md:max-w-[45rem] md:gap-5 md:flex-row">
-
-                <div className="flex flex-row items-center gap-5">
-                    <SwitchOption
-                        selectedValue={typeList}
-                        setSelectedValue={setTypeList}
-                        options={[
-                            <SliderListIcon className="text-main" size={18}/>,
-                            <ColumnsListIcon className="text-main" size={18}/>
-                        ]}
-                        values={["SLIDER", "COLUMNS"]}
-                    />
-                    <p className="
-                        text-base font-semibold hidden  
-                        text-subtext
-                        md:block">
-                            LISTA DE PROYECTOS
-                    </p>
-
-                </div>
-                {/* <button className="text-xs cursor-pointer gradient-menu rounded-lg px-4 border-1
-                text-subtext hover:text-main border-main/10">
-                    VER TODOS +
-                </button> */}
-            </div>
-
-            {typeList == "SLIDER" ? <SliderList/> : <ColumnList/>}
+            <ColumnList/>
             <StarBackground widthBackground={70} heightBackground={80} starCount={80} topOffset={220} />
 
         </section>
